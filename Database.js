d0ac5787546251019b09806e999d8754d1bc411b
zixuan-pei <peizx28@icloud.com>
2022-02-13 13:08:41 -0800

    test

---
+++ Database.js
@@ -0,0 +1,44 @@
+const { MongoClient, ObjectID } = require('mongodb');	// require the mongodb driver
+
+/**
+ * Uses mongodb v3.6+ - [API Documentation](http://mongodb.github.io/node-mongodb-native/3.6/api/)
+ * Database wraps a mongoDB connection to provide a higher-level abstraction layer
+ * for manipulating the objects in our cpen322 app.
+ */
+function Database(mongoUrl, dbName){
+    if (!(this instanceof Database)) return new Database(mongoUrl, dbName);
+    this.connected = new Promise((resolve, reject) => {
+        MongoClient.connect(
+            mongoUrl,
+            {
+                useNewUrlParser: true
+            },
+            (err, client) => {
+                if (err) reject(err);
+                else {
+                    console.log('[MongoClient] Connected to ' + mongoUrl + '/' + dbName);
+                    resolve(client.db(dbName));
+                }
+            }
+        )
+    });
+    this.status = () => this.connected.then(
+        db => ({ error: null, url: mongoUrl, db: dbName }),
+        err => ({ error: err })
+    );
+}
+
+Database.prototype.getData = function(){
+    return this.connected.then(db =>
+        new Promise((resolve, reject) => {
+            db.collection('some collection').find({}).toArray((err, data) => {
+                if (err)
+                    reject(err);
+                else
+                    resolve(data);
+            })
+        })
+    )
+}
+
+module.exports = Database;
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
+const Database = require('./Database');
+
 
 const PORT = process.env.PORT || 3000;
+const mongodbUrl = 'mongodb+srv://instance-0:'+process.env.MONGO_PASSWORD+'@cluster0.xuiqx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const mongodbName = "cpen322-messenger";
+const db = new Database(mongodbUrl, mongodbName);
 
 app.get('/', (req, res) => {
-    res.send("Welcome to the home page.");
+    res.send(mongodbUrl);
 });
+
+
 
 app.listen(PORT, () => {
     console.log("Application starts.");
