8b2da620571873111d5125b6dc6eccd22d3f967f
zixuan-pei <peizx28@icloud.com>
2022-04-04 11:32:24 -0700

    add new endpoint

--- APIs/data.js
+++ APIs/data.js
@@ -6,18 +6,26 @@ const router = express.Router();
 let co2 = 0;
 let people = 0;
 
-let device = "device1"
-
-let add_data = function () {
-    let dataWithTime = {
-        "time": Date.now(),
-        "co2": co2,
-        "people": people
+let devices = {
+    "device1": {
+        "co2": 0, 
+        "people": 0
+    },
+    "device2": {
+        "co2": 0, 
+        "people": 0
     }
-    db_data.addData(device, dataWithTime)
 }
 
-setInterval(add_data, 60000);
+let add_data = function () {
+    Object.keys(devices).forEach(device => {
+        console.log({"time": Date.now(), ...devices[device]})
+        if (devices[device].co2 !== 0)
+            db_data.addData(device, {"time": Date.now(), ...devices[device]})
+    })
+}
+
+setInterval(add_data, 6000);
 
 router.get('/:id', (req, res) => {    
     let id = req.params.id;
@@ -33,29 +41,22 @@ router.get('/:id', (req, res) => {
 router.post('/:id/:type', (req, res) => {
     let id = req.params.id;
     let type = req.params.type;
-    if(type === 'co2')
+    if(type === 'co2') {
+        devices[id].co2 = req.body.co2;
         co2 = req.body.co2;
-    else if(type === 'people')
+    } else if(type === 'people') {
+        devices[id].people = req.body.people;
         people = req.body.people;
+    }
 
     res.send("OK");
     
 });
 
-// router.get('/:id', (req, res) => {
-//     let id = req.params.id;
-//     db_data.addData(id).then(devices => {
-//         let id = req.params.id;
-//         let device = devices.find(device => device._id === id);
-//         res.send(device);
-//     }).catch(err => {
-//         console.log(err);
-//         res.send(err);
-//     });
-// });
-
-router.put('/', (req, res) => {
-
+router.get('/:id/new', (req, res) => {
+    let id = req.params.id;
+    res.send(devices[id]);
 });
+
 
 module.exports = router;
